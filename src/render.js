e9093332cf752f020c88dd98c941a70a03dcfa19
artem-iliasov <iliasov.artem.27@gmail.com>
2019-02-20 21:26:55 +0300

    add validation and rss feed

---
+++ src/render.js
@@ -0,0 +1,12 @@
+export default (data) => {
+  const { title, description, items } = data;
+  const div = document.querySelector('.feed');
+  const rssTitle = `<h2>${title}</h2><p>${description}}</p>`;
+  const rssFeed = `<ul>${items.map((item) => {
+    const itemTitle = item.querySelector('title').textContent;
+    const itemLink = item.querySelector('link').textContent;
+    return `<li><a href="${itemLink}">${itemTitle}</a></li>`;
+  }).join('')
+  }</ul>`;
+  div.innerHTML = `${rssTitle}${rssFeed}`;
+};
--- src/rss.js
+++ src/rss.js
@@ -1,36 +1,54 @@
 import validator from 'validator';
 import axios from 'axios';
 import WatchJS from 'melanke-watchjs';
+import renderHTML from './render';
 
-const watch = WatchJS.watch;
+const { watch } = WatchJS;
 const corsHost = 'https://cors-anywhere.herokuapp.com/';
 const parse = new DOMParser();
-
-const isURL = str => validator.isURL(str);
 
 export default () => {
   const state = {
     inputValue: '',
     inputValid: false,
-    rssFeed: [],
+    currentRssChannel: '',
+    rss: {},
+    rssTitle: '',
   };
   const input = document.getElementById('source');
   input.addEventListener('input', (e) => {
     e.preventDefault();
-    const { value } = e.target.value;
+    const { value } = e.target;
     state.inputValue = value;
-    //state.inputValid = isURL(value);
+    state.inputValid = validator.isURL(value) && value !== state.currentRssChannel;
+    console.log(`${state.inputValid}${state.inputValue}`);
   });
   const form = document.querySelector('form');
   form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(input.value);
     axios.get(`${corsHost}${input.value}`).then((response) => {
+      state.currentRssChannel = input.value;
       const parsedResponse = parse.parseFromString(response.data, 'application/xml');
-      const title = parsedResponse.querySelector('title');
-      const description = parsedResponse.querySelector('description');
-      const items = parsedResponse.querySelectorAll('item');
-      console.log([title, description, items]);
+      const title = parsedResponse.querySelector('title').textContent;
+      const description = parsedResponse.querySelector('description').textContent;
+      const items = Array.from(parsedResponse.querySelectorAll('item'));
+      state.rss = { title, description, items };
+      state.rssTitle = state.rss.title;
+      state.inputValue = '';
+      input.value = '';
+      state.inputValid = false;
     });
+  });
+  watch(state, 'inputValue', () => {
+    if (state.inputValid) {
+      document.getElementById('source').className = 'form-control is-valid';
+      document.querySelector('button').disabled = false;
+    } else {
+      document.getElementById('source').className = 'form-control is-invalid';
+      document.querySelector('button').disabled = true;
+    }
+  });
+  watch(state, 'rssTitle', () => {
+    renderHTML(state.rss);
   });
 };
