7c5904a2559c4225152a73da22f8e5453dd3d4ec
artem-iliasov <iliasov.artem.27@gmail.com>
2019-02-21 20:57:44 +0300

    fix

--- src/render.js
+++ src/render.js
@@ -1,7 +1,7 @@
 export default (data) => {
   const { title, description, items } = data;
   const div = document.querySelector('.feed');
-  const rssTitle = `<h2>${title}</h2><p>${description}}</p>`;
+  const rssTitle = `<h2>${title}</h2><p>${description}</p>`;
   const rssFeed = `<ul>${items.map((item) => {
     const itemTitle = item.querySelector('title').textContent;
     const itemLink = item.querySelector('link').textContent;
