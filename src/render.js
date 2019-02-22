a106597936115c333d2208c0d6e4b973e692c1fe
artem-iliasov <iliasov.artem.27@gmail.com>
2019-02-22 20:06:40 +0300

    add modal

--- src/render.js
+++ src/render.js
@@ -4,13 +4,14 @@ export default (data) => {
   const { title, description, items } = data;
   const div = document.querySelector('.feed');
   const rssTitle = `<h2>${title}</h2><p>${description}</p><br><br>`;
-  const rssFeed = `<ul class="list-group-flush justify-content-left">${items.map((item) => {
+  const rssFeed = `<ul class="list-group-flush justify-content-left">${items.map((item, index) => {
     const itemTitle = item.querySelector('title').textContent;
     const itemLink = item.querySelector('link').textContent;
     const itemDesription = item.querySelector('description').textContent;
-    const itemPubDate = item.querySelector('pubDate');
-    const btnId = Math.round(new Date(itemPubDate).getTime() / 1000);
-    const modal = renderModal(itemDesription, btnId);
+    //  const itemPubDate = item.querySelector('pubDate').textContent;
+    //  const btnId = Math.round(new Date(itemPubDate).getTime() / 1000).toString();
+    console.log(index);
+    const modal = renderModal(itemTitle, itemDesription, index);
     return `<li class="list-group-item py-2">${modal}<a href="${itemLink}">      ${itemTitle}</a></li>`;
   }).join('')
   }</ul>`;
--- src/renderModal.js
+++ src/renderModal.js
@@ -1,12 +1,12 @@
-export default (description, id) => (`
-  <button type="button" class="btn btn-info btn-sm" data-toggle="modal" data-target="#${id}">More</button>
-  <div class="modal" id=${id}>
+export default (title, description, id) => (`
+  <button type="button" class="btn btn-info btn-sm" data-toggle="modal" data-target="#myModal-${id}">More</button>
+  <div class="modal fade" id=myModal-${id}>
   <div class="modal-dialog">
     <div class="modal-content">
     
       <!-- Modal Header -->
       <div class="modal-header">
-        <h4 class="modal-title"></h4>
+        <h4 class="modal-title">${title}</h4>
         <button type="button" class="close" data-dismiss="modal">&times;</button>
       </div>
       
